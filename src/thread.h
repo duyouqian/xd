a803f3dd47d0cb2f01163bc2086b2e8adaf9c04d
duyq <356308751@qq.com>
2015-03-10 10:00:24 +0800

    + 添加 thread.h

---
+++ src/thread.h
@@ -0,0 +1,15 @@
+#ifndef XD_THREAD_H
+#define XD_THREAD_H
+
+#ifdef __unix__
+    // unix or linux
+    #include "thread_linux.h"
+    typedef XDThreadLinuxImp XDThread;
+#elif __APPLE__
+    // mac os
+#else
+    // win
+#endif
+
+
+#endif // end xd_thread_h
