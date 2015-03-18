d2b5a4e1c92c6a8b8e01fe54d361d6725b60e8d8
duyq <356308751@qq.com>
2015-03-18 19:05:45 +0800

    + 添加io事件循环(未实现)

---
+++ src/event_loop.cpp
@@ -0,0 +1,17 @@
+#include "event_loop.h"
+#include "log.h"
+#include "thread.h"
+
+XDIOEventLoop::XDIOEventLoop()
+             : threadID_(XDThread::getCurrentThreadID())
+{
+}
+
+XDIOEventLoop::~XDIOEventLoop()
+{
+}
+
+void XDIOEventLoop::loop()
+{
+    XDLOG_minfo("[IOEventLoop] loop run threadID:%u", threadID_);
+}
---
+++ src/event_loop.h
@@ -0,0 +1,21 @@
+#ifndef XD_EVENT_LOOP_H
+#define XD_EVENT_LOOP_H
+
+#include "noncopyable.h"
+//#include "poller.h"
+#include "types.h"
+
+// IO事件循环
+class XDIOEventLoop : public XDNoncopyable
+{
+public:
+    XDIOEventLoop();
+    ~XDIOEventLoop();
+    // 主循环 会调用poller->poll来获取IO事件
+    void loop();
+private:
+    //XDPoller *poller_;
+    uint32 threadID_;
+};
+
+#endif // end xd_event_loop_h
