b4d4dc2ef0cf356ba27dca478ea08c704a39baba
duyq <356308751@qq.com>
2015-03-19 22:10:21 +0800

    * 修改IO事件回调处理方式

--- src/callback.h
+++ src/callback.h
@@ -3,16 +3,31 @@
 
 #include "shareable.h"
 #include "shared_pointer.h"
+#include "types.h"
 
+// 无参数回调
+class XDIOEventCallBack : public XDShareable
+{
+public:
+    virtual bool exec() = 0;
+};
+
+// 新连接回调
+class XDIOEventConnectionCallBack : public XDShareable
+{
+public:
+    virtual bool exec(/*const XDTcpConnectionPtr &conn*/) = 0;
+};
+
+// 读回调
 class XDIOEventReadCallBack : public XDShareable
 {
 public:
-    virtual bool exec()
-    {
-        return true;
-    }
+    virtual bool exec(uint64 timestamp) = 0;
 };
 
+typedef XDSharedPtr<XDIOEventCallBack> XDIOEventCallBackPtr;
+typedef XDSharedPtr<XDIOEventConnectionCallBack> XDIOEventConnectionCallBackPtr;
 typedef XDSharedPtr<XDIOEventReadCallBack> XDIOEventReadCallBackPtr;
 
 #endif // end xd_callback_h
